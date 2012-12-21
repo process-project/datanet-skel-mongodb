1b1fa2b90326331ef9e8367fb030e036422f2820
Marek Kasztelnik <mkasztelnik@gmail.com>
2012-12-21 11:39:40 +0100

    example model correction, json dependency added to the application

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,11 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'boot'
+require 'json'
 
 Bundler.require :default, ENV['RACK_ENV']
 
 require 'datanet-skel'
 require 'mongo'
 require 'mapper'
+require 'json'
